412fe858f5685edfef5e016aa324340d8efd42ff
Coder_Liu <2396923396@qq.com>
2026-04-02 10:47:19 +0800

    fix: opencode-linux-x64/bin/app/dist update

--- opencode-linux-x64/bin/app/dist/assets/ghostty-web-CpO5N1TZ.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fb66123e174d87a11b6c8963573d6a5098b01f8d20f96009f13d93173c40fa5d
-size 638369
--- opencode-linux-x64/bin/app/dist/assets/home-CvdJM1R8.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:07fe0e76250f8ef98fc06716e2754795f4f5589a7fb6db9c211315b10999bc76
-size 2833
